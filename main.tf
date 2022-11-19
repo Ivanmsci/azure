76de3bd6d5bd9ae82ed969560511ff4b97ae654e
Ivan Madrazo <ivanm.sci81@gmail.com>
2022-11-19 09:27:48 -0600

    security group and rules

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "azurerm_resource_group" "mtc-rg" {
 }
 
 resource "azurerm_virtual_network" "mtc-vn" {
-  name                = "mtc-network" 
+  name                = "mtc-network"
   resource_group_name = azurerm_resource_group.mtc-rg.name
   location            = azurerm_resource_group.mtc-rg.location
   address_space       = ["10.123.0.0/16"]
@@ -31,8 +31,39 @@ resource "azurerm_virtual_network" "mtc-vn" {
 }
 
 resource "azurerm_subnet" "mtc_subnet" {
-  name = "mtc_subnet"
-  resource_group_name = azurerm_resource_group.mtc-rg.name 
+  name                 = "mtc_subnet"
+  resource_group_name  = azurerm_resource_group.mtc-rg.name
   virtual_network_name = azurerm_virtual_network.mtc-vn.name
-  address_prefixes = ["10.123.1.0/24"]
+  address_prefixes     = ["10.123.1.0/24"]
+}
+
+
+resource "azurerm_network_security_group" "mtc-sg" {
+  name                = "mtc-sg"
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+
+  tags = {
+
+    environment = "dev"
+
+
+  }
+
+
+}
+
+
+resource "azurerm_network_security_rule" "mtc-dev-rule" {
+  name                        = "mtc-dev-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "189.163.110.109/32"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.mtc-sg.name
+  resource_group_name         = azurerm_resource_group.mtc-rg.name
 }
