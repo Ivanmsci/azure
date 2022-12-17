2f507ea4bc1a7cfdef541f51ed33745dfda6a983
Ivan Madrazo <ivanm.sci81@gmail.com>
2022-12-16 23:20:55 -0600

    adding the ssh output

---
+++ outputs.tf
@@ -0,0 +1,8 @@
+output "connection_script" {
+
+     value = templatefile("${var.host_os}-ssh-script.tpl", {
+         hostname = azurerm_linux_virtual_machine.mtc-vm.public_ip_address,
+         user = "adminuser",
+         identityfile = "~/.ssh/id_rsa.pub"
+      })
+}
