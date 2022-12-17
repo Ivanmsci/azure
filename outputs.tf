fe6eb951498a796371cd6d77851b0239e332f434
Ivan Madrazo <ivanm.sci81@gmail.com>
2022-12-16 22:06:40 -0600

    aading an aputput for ssh conncetion

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "connection_script" {
   value = templatefile("${var.host_os}-ssh-script.tpl", {
-         hostname = azurerm_network_interface.mtc-nic.public_ip_address,
+         hostname = azurerm_linux_virtual_machine.mtc-vm.public_ip_address,
          user = "adminuser",
          identityfile = "~/.ssh/id_rsa.pub"
       })
