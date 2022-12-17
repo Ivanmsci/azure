f38d49256278a0f08a032d4416149c5342ec903f
Ivan Madrazo <ivanm.sci81@gmail.com>
2022-12-16 23:36:34 -0600

    adding output for the ip address

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
+output "mtc_ip_address" {
+
+    value = azurerm_linux_virtual_machine.mtc-vm.public_ip_address
+}
+
+
 output "connection_script" {
 
      value = templatefile("${var.host_os}-ssh-script.tpl", {
